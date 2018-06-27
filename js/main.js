23dbe6ebdd8e3d6e84302a39e35dd5ac33c31677
Evelyn Galvez <evelyn.galvez.p@gmail.com>
2018-06-27 19:59:51 -0400

    agregada función validación email

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-/* Holiii acá va tu código también */
+const email = document.getElementById('exampleInputEmail1').value;
+
+const validarEmail = (email) => {
+  let regex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  return regex.test(email) ? true : false;
+}
+
+ 
+if(validar_email(email)) {
+    alert("El email es correcto");
+  } else {
+    alert("El email NO es correcto");
+  }
+
